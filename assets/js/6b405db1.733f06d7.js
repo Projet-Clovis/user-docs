d4b632c3f51a5c3572090636bb0d7b9034af3b30
Ab2nour <briochekine@hotmail.fr>
2022-02-23 16:32:30 +0100

    Deploy website - based on ec5ecb30203d8a80fccf6cf036061cb0bb8eb345

---
+++ assets/js/6b405db1.733f06d7.js
@@ -0,0 +1,1 @@
+(self.webpackChunkwebsite_dev=self.webpackChunkwebsite_dev||[]).push([[705],{9239:function(e){"use strict";e.exports=JSON.parse('{"permalink":"/user-docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/814f3328.369a3d8e.js
@@ -0,0 +1,1 @@
+(self.webpackChunkwebsite_dev=self.webpackChunkwebsite_dev||[]).push([[535],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"test","permalink":"/user-docs/blog/test"}]}')}}]);
